8867fe60b02b1819dacd5f7c62b653df55d35e17
Ariel Barclay <mrsbarclay101008@gmail.com>
2020-07-10 14:59:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,7 +23,7 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.each}"
+    puts "Currently serving #{katz_deli.}"
     katz_deli.shift
   end
 end
